358f5d57725d1440f03196c863d7b054034ea313
wudada <2567199685@qq.com>
2022-03-26 13:45:13 +0800

    complete three icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,65 @@
-import React from 'react';
-import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
-import Money from './views/Money';
-import Statistics from './views/Statistics';
-import Tags from './views/Tags';
-import NoMatch from './views/NoMatch';
+import React from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import styled from "styled-components";
+import Nav from "components/Nav";
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  flex-grow: 1;
+`;
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/tags">
-          <Tags/>
-        </Route>
-        <Route path="/money">
-          <Money/>
-        </Route>
-        <Route path="/statistics">
-          <Statistics/>
-        </Route>
-        <Redirect exact from="/" to="/money"/>
-        <Route path="*">
-          <NoMatch/>
-        </Route>
-      </Switch>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Route exact path="/">
+              <Redirect to="/tags" />
+            </Route>
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav />
+      </Wrapper>
     </Router>
   );
 }
 
+function NoMatch() {
+  return <div>页面不存在，你丫输错地址了吧！</div>;
+}
+
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Money() {
+  return <h2>记账页</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页</h2>;
+}
 
 export default App;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,23 +1,24 @@
-import styled from 'styled-components';
-import {Link} from 'react-router-dom';
-import React from 'react';
-import Icon from './Icon';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import React from "react";
+require("components/icons/tags.svg"); // svgo-loader => svg-sprite-loader
+require("components/icons/money.svg");
+require("components/icons/statistics.svg");
 
 const NavWrapper = styled.nav`
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   > ul {
-    display:flex;
-    > li{
-      width: 33.3333%;
-      text-align:center;
+    display: flex;
+    > li {
+      width: 33.33%;
+      text-align: center;
       display: flex;
       flex-direction: column;
-      padding: 4px 0;
-      justify-content: center;
       align-items: center;
+      padding: 4px p;
       .icon {
-        width: 24px;      
+        width: 24px;
         height: 24px;
       }
     }
@@ -29,20 +30,26 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Icon name="tag"/>
+          {/* <img src={x} alt="" /> */}
+          <svg fill="green" className="icon">
+            <use xlinkHref="#tags" />
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
-          <Icon name="money"/>
+          <svg fill="green" className="icon">
+            <use xlinkHref="#money" />
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
-          <Icon name="chart"/>
+          <svg fill="green" className="icon">
+            <use xlinkHref="#statistics" />
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
     </NavWrapper>
   );
 };
-
 export default Nav;
