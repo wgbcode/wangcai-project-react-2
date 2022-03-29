3c9d8fa8e99e7de3e4075b96f77dc1e7c8cd922d
wudada <2567199685@qq.com>
2022-03-29 10:28:08 +0800

    complete Tags.tsx style

--- src/views/Money/NumberSection.tsx
+++ src/views/Money/NumberSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import generateOutput from "./NumberSection/generateOutput";
 import Wrapper from "./NumberSection/Wrapper";
 
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,10 +1,62 @@
+import Icon from "components/Icon";
 import React from "react";
+import styled from "styled-components";
+import useTags from "useTag";
 import Layout from "../components/Layout";
 
+const TagList = styled.ol`
+  background: #ffffff;
+  font-size: 16px;
+  > li {
+    padding: 12px 16px 16px 0;
+    margin-left: 16px;
+    border-bottom: 3px solid #dddde0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+const Button = styled.button`
+  font-size: 18px;
+  color: white;
+  background: #767676;
+  border: none;
+  padding: 8px 14px;
+  /* margin-top: 54px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto; */
+  border-radius: 4px;
+`;
+
+const Center = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+const Span = styled.div`
+  height: 27px;
+`;
+
 function Tags() {
+  // eslint-disable-next-line
+  const { tags, setTags } = useTags();
   return (
     <Layout>
-      <h2>标签页</h2>
+      <TagList>
+        {tags.map((tag) => (
+          <li key={tag}>
+            <span className="onLine">{tag}</span>
+            <Icon name="tagArrows" />
+          </li>
+        ))}
+      </TagList>
+      <Center>
+        <Span />
+        <Span />
+        <Button>新增标签</Button>
+      </Center>
     </Layout>
   );
 }
