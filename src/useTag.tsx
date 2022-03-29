90c35b64ad48b5e0b6f00bdc2fac633534d73912
wudada <2567199685@qq.com>
2022-03-29 13:23:43 +0800

    create createId function, let id self-incrementing

--- src/useTag.tsx
+++ src/useTag.tsx
@@ -1,12 +1,13 @@
+import createId from "lib/createId";
 import { useState } from "react";
 
 // 封装一个自定义 Hook
 const useTags = () => {
   const [tags, setTags] = useState<{ id: number; name: string }[]>([
-    { id: 0, name: "衣" },
-    { id: 1, name: "食" },
-    { id: 2, name: "住" },
-    { id: 3, name: "行" },
+    { id: createId(), name: "衣" },
+    { id: createId(), name: "食" },
+    { id: createId(), name: "住" },
+    { id: createId(), name: "行" },
   ]);
   return { tags, setTags };
 };
